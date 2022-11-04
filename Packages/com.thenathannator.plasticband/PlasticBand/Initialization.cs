c992e85d94117eda9c7abd852af4783fc23f41b5
Nathan <nathanhurst03@gmail.com>
2022-11-04 09:06:09 -0600

    Add an Initialization class to handle package initialization

---
+++ Packages/com.thenathannator.plasticband/PlasticBand/Initialization.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace PlasticBand
+{
+    /// <summary>
+    /// Handles initialization of the package.
+    /// </summary>
+#if UNITY_EDITOR
+    [InitializeOnLoad]
+#endif
+    internal static class Initialization
+    {
+#if UNITY_EDITOR
+        static Initialization()
+        {
+            Initialize();
+        }
+#endif
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        internal static void Initialize()
+        {
+        }
+    }
+}
