057f1d1d047b062a123afcc6255c3c5fcd1d3d5e
Nathan <nathanhurst03@gmail.com>
2023-01-12 02:09:29 -0700

    Register XInput 4-lane kit normally and move fixup registration to 5-lane

--- Packages/com.thenathannator.plasticband/PlasticBand/Devices/FourLaneDrumkit/XInputFourLaneDrumkit.cs
+++ Packages/com.thenathannator.plasticband/PlasticBand/Devices/FourLaneDrumkit/XInputFourLaneDrumkit.cs
@@ -74,19 +74,7 @@ namespace PlasticBand.Devices
 
         internal new static void Initialize()
         {
-#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
-            InputSystem.RegisterLayout<XInputFourLaneDrumkit>();
-            // 4-lane kits and 5-lane kits share the same subtype, they need to be differentiated in another way
-            // 5-lane kits always hold the left-stick click input, 4-lane kits use that for the second kick but
-            // realistically that isn't likely to be held when powering on
-            // May be some more specific capability data that also distinguishes them, but that probably isn't reliable
-            XInputLayoutFixup.RegisterLayoutResolver(XInputController.DeviceSubType.DrumKit, (capabilities, state) => {
-                if ((state.buttons & (ushort)XInputGamepad.Button.LeftThumb) != 0)
-                    return typeof(XInputFiveLaneDrumkit).Name;
-
-                return typeof(XInputFourLaneDrumkit).Name;
-            });
-#endif
+            XInputDeviceUtils.Register<XInputFourLaneDrumkit>(XInputController.DeviceSubType.DrumKit);
         }
 
         /// <summary>
