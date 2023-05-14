f72d124c126483e926bcc213a7c0763cce0e51b6
Nathan <nathanhurst03@gmail.com>
2023-05-13 18:23:01 -0600

    Add Santroller stage kit layout

---
+++ Packages/com.thenathannator.plasticband/PlasticBand/Devices/StageKit/SantrollerHidStageKit.cs
@@ -0,0 +1,100 @@
+using System.Runtime.InteropServices;
+using PlasticBand.Haptics;
+using PlasticBand.LowLevel;
+using UnityEngine.InputSystem.Haptics;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Utilities;
+
+// PlasticBand reference doc:
+// TODO
+
+namespace PlasticBand.Devices
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    internal unsafe struct SantrollerHidStageKitState : IInputStateTypeInfo
+    {
+        public FourCC format => HidDefinitions.InputFormat;
+
+        public byte reportId;
+
+        [InputControl(name = "buttonWest", layout = "Button", bit = 0, displayName = "Square")]
+        [InputControl(name = "buttonSouth", layout = "Button", bit = 1, displayName = "Cross")]
+        [InputControl(name = "buttonEast", layout = "Button", bit = 2, displayName = "Circle")]
+        [InputControl(name = "buttonNorth", layout = "Button", bit = 3, displayName = "Triangle")]
+
+        [InputControl(name = "selectButton", layout = "Button", bit = 8, displayName = "Select")]
+        [InputControl(name = "startButton", layout = "Button", bit = 9)]
+
+        [InputControl(name = "psButton", layout = "Button", bit = 12, displayName = "PlayStation")]
+        public ushort buttons;
+
+        [InputControl(name = "dpad", layout = "Dpad", format = "BIT", sizeInBits = 4, defaultState = 8)]
+        [InputControl(name = "dpad/up", layout = "DiscreteButton", format = "BIT", bit = 0, sizeInBits = 4, parameters = "minValue=7,maxValue=1,nullValue=8,wrapAtValue=7")]
+        [InputControl(name = "dpad/right", layout = "DiscreteButton", format = "BIT", bit = 0, sizeInBits = 4, parameters = "minValue=1,maxValue=3")]
+        [InputControl(name = "dpad/down", layout = "DiscreteButton", format = "BIT", bit = 0, sizeInBits = 4, parameters = "minValue=3,maxValue=5")]
+        [InputControl(name = "dpad/left", layout = "DiscreteButton", format = "BIT", bit = 0, sizeInBits = 4, parameters = "minValue=5, maxValue=7")]
+        public byte dpad;
+
+        public fixed byte unused3[24];
+    }
+
+    [InputControlLayout(stateType = typeof(SantrollerHidStageKitState), displayName = "Santroller HID Stage Kit")]
+    internal class SantrollerHidStageKit : StageKit, ISantrollerHaptics
+    {
+        internal new static void Initialize()
+        {
+            SantrollerLayoutFinder.RegisterHIDLayout<SantrollerHidStageKit>(SantrollerDeviceType.StageKit);
+        }
+
+        protected override void FinishSetup()
+        {
+            base.FinishSetup();
+            m_Haptics = new HidSantrollerHaptics(this);
+        }
+
+        private new SantrollerHaptics m_Haptics;
+
+        /// <inheritdoc cref="IHaptics.PauseHaptics()"/>
+        public override void PauseHaptics() => m_Haptics.PauseHaptics();
+
+        /// <inheritdoc cref="IHaptics.ResumeHaptics()"/>
+        public override void ResumeHaptics() => m_Haptics.ResumeHaptics();
+
+        /// <inheritdoc cref="IHaptics.ResetHaptics()"/>
+        public override void ResetHaptics() => m_Haptics.ResetHaptics();
+
+        /// <inheritdoc cref="IStageKitHaptics.SetFogMachine(bool)"/>
+        public override void SetFogMachine(bool enabled) => m_Haptics.SetFogMachine(enabled);
+
+        /// <inheritdoc cref="IStageKitHaptics.SetStrobeSpeed(StageKitStrobeSpeed)"/>
+        public override void SetStrobeSpeed(StageKitStrobeSpeed speed) => m_Haptics.SetStrobeSpeed(speed);
+
+        /// <inheritdoc cref="IStageKitHaptics.SetLeds(StageKitLedColor, StageKitLed)"/>
+        public override void SetLeds(StageKitLedColor color, StageKitLed leds) => m_Haptics.SetLeds(color, leds);
+
+        /// <inheritdoc cref="IStageKitHaptics.SetRedLeds(StageKitLed)"/>
+        public override void SetRedLeds(StageKitLed leds) => SetLeds(StageKitLedColor.Red, leds);
+
+        /// <inheritdoc cref="IStageKitHaptics.SetYellowLeds(StageKitLed)"/>
+        public override void SetYellowLeds(StageKitLed leds) => SetLeds(StageKitLedColor.Yellow, leds);
+
+        /// <inheritdoc cref="IStageKitHaptics.SetBlueLeds(StageKitLed)"/>
+        public override void SetBlueLeds(StageKitLed leds) => SetLeds(StageKitLedColor.Blue, leds);
+
+        /// <inheritdoc cref="IStageKitHaptics.SetGreenLeds(StageKitLed)"/>
+        public override void SetGreenLeds(StageKitLed leds) => SetLeds(StageKitLedColor.Green, leds);
+
+        /// <inheritdoc cref="ISantrollerHaptics.SetStarPowerFill(float)"/>
+        public void SetStarPowerFill(float fill) => m_Haptics.SetStarPowerFill(fill);
+
+        /// <inheritdoc cref="ISantrollerHaptics.SetStarPowerActive(bool)"/>
+        public void SetStarPowerActive(bool enabled) => m_Haptics.SetStarPowerActive(enabled);
+
+        /// <inheritdoc cref="ISantrollerHaptics.SetMultiplier(uint)"/>
+        public void SetMultiplier(uint multiplier) => m_Haptics.SetMultiplier(multiplier);
+
+        /// <inheritdoc cref="ISantrollerHaptics.SetSolo(bool)"/>
+        public void SetSolo(bool enabled) => m_Haptics.SetSolo(enabled);
+    }
+}
