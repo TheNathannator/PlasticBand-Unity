ec836260ce10ee9dc992e602833d609ae38417af
Nathan <nathanhurst03@gmail.com>
2023-10-30 02:07:56 -0600

    Remove now-unused ButtonAxisPair control

--- Packages/com.thenathannator.plasticband/PlasticBand/Initialization.cs
+++ Packages/com.thenathannator.plasticband/PlasticBand/Initialization.cs
@@ -31,7 +31,6 @@ namespace PlasticBand
             XInputLayoutFinder.Initialize();
 
             // General controls
-            ButtonAxisPairControl.Initialize();
             MaskButtonControl.Initialize();
 
             // 5-fret guitars
