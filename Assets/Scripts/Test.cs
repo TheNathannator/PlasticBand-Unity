5cf8281a1d9405c7fcd50f8be01812c1d52f3e16
Nathan <nathanhurst03@gmail.com>
2023-11-02 19:20:39 -0600

    Change input debug scene to a general test scene

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+using PlasticBand;
+using PlasticBand.Devices;
+using PlasticBand.Haptics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Small script for testing the APIs provided by the device classes.
+// This class is persisted so it doesn't need to be created repeatedly, just modified.
+// No changes to this class should be committed, aside from common routines or quality-of-life additions.
+
+namespace PlasticBand.Test
+{
+    public class Test : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
